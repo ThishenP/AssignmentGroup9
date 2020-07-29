20b0c63f121a4d4fc8246e61587407a0f0169f33
ThishenP <Thishenp@gmail.com>
2020-07-29 17:50:00 +0200

    basic ui

---
+++ Assets/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void HowTo()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
